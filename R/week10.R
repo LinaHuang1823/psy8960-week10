8fa88e117cef8d4646bef8471f2533c252bdc838
Lina Huang <huan2384@umn.edu>
2023-03-31 11:55:02 -0500

    visualization: step 6

--- R/week10.R
+++ R/week10.R
@@ -9,3 +9,10 @@ gss_tbl<-gss_tbl %>%
   filter(!(HRS1 %in% c(98, 99)))%>%
   select_if(function(x) mean(x %in% c(98, 99)) < 0.75)
 
+#Visualization
+ggplot(gss_tbl, aes(x = HRS1)) +
+  geom_histogram(binwidth = 1, color = "black", fill = "dodgerblue", alpha = 0.8) +
+  labs(title = "Distribution of Workhours",
+       x = "Workhours per Week",
+       y = "Frequency")+
+  theme_minimal() 
