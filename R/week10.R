d90c8d0dafa5d69d487a29c764b75e790e7604c6
Lina Huang <huan2384@umn.edu>
2023-04-03 01:30:02 -0500

    update import&cleaning and visualization

--- R/week10.R
+++ R/week10.R
@@ -2,17 +2,26 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(haven)
+library(caret)
+library(glmnet)
+library(randomForest)
 
-# Data Import
+# Data Import and Cleaning
 gss_tbl<-read_sav("../data/GSS2016.sav")
-gss_tbl<-gss_tbl %>% 
-  filter(!(HRS1 %in% c(98, 99)))%>%
-  select_if(function(x) mean(x %in% c(98, 99)) < 0.75)
+
+gss_tbl <- gss_tbl%>% #Mark any missing, don’t know, inapplicable, or otherwise not-clearly-answered items as missing values. 
+  mutate(across(everything(), ~na_if(., -1))) %>% 
+  mutate(across(everything(), ~na_if(., -2))) %>% 
+  mutate(across(everything(), ~na_if(., -3)))%>% 
+  mutate(across(everything(), ~na_if(., -4)))%>%
+  filter(!is.na(HRS2))%>% #Remove anyone who has a missing value for HRS2
+  select_if(~sum(!is.na(.))/length(.) >= 0.75) #retain only variables with less than 75% missingness
 
 #Visualization
-ggplot(gss_tbl, aes(x = HRS1)) +
-  geom_histogram(binwidth = 1, color = "black", fill = "dodgerblue", alpha = 0.8) +
+ggplot(gss_tbl, aes(x = as.numeric(HRS2))) +
+  geom_histogram(binwidth = 5, fill = "blue", color = "black", alpha = 0.7) +
   labs(title = "Distribution of Workhours",
        x = "Workhours per Week",
-       y = "Frequency")+
-  theme_minimal() 
+       y = "Frequency")
+ 
+
