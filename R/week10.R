f62b5e6144369e10934a41e9cc5b4bbde4aaeb26
Lina Huang <huan2384@umn.edu>
2023-04-03 03:21:51 -0500

    update analysis part

--- R/week10.R
+++ R/week10.R
@@ -29,17 +29,20 @@ row_tbl<-sample(nrow(gss_tbl))
 gss_tbl<-gss_tbl[row_tbl,]
 # Preprocess the data
 gss_tbl_processed <- gss_tbl %>% 
-  mutate(across(where(is.numeric), ~ ifelse(is.na(.), median(., na.rm = TRUE), .)))
+  mutate(across(where(is.numeric), ~ ifelse(is.na(.),median(., na.rm = TRUE),.)))
 # Create train-test split (75/25)
 train_idx <- createDataPartition(gss_tbl_processed$HRS2, p = 0.75, list = FALSE)
-gss_train_tbl <- gss_tbl_processed[train_idx, ]
-gss_test_tbl <- gss_tbl_processed[-train_idx, ]
+gss_train_tbl <- gss_tbl_processed[train_idx, ] #training set
+gss_test_tbl <- gss_tbl_processed[-train_idx, ] #test set
 # Set up cross-validation
 train_control <- trainControl(method = "cv", number = 10)
-# Set Hyperparameter grids
+# Set hyperparameter grids for elastic net
 elastic_net_grid <- expand.grid(alpha = seq(0, 1, length.out = 5),
                                 lambda = seq(0.001, 0.1, length.out = 5))
-random_forest_grid <- expand.grid(mtry = seq(2, floor(sqrt(ncol(gss_train_tbl))), length.out = 5))
+# Set hyperparameter grids for random forest
+random_f_grid <- expand.grid(mtry = seq(2, floor(sqrt(ncol(gss_train_tbl))),
+                                             length.out = 5))
+# Set hyperparameter grids for eXtreme Gradient Boosting
 xgboost_grid <- expand.grid(nrounds = c(100),
                             max_depth = seq(3, 10, by = 3),
                             eta = c(0.01, 0.1, 0.3),
@@ -50,11 +53,17 @@ xgboost_grid <- expand.grid(nrounds = c(100),
 # Train OLS, elastic net, random forest, and xgboost models with grid search
 formula <- HRS2 ~ .
 models <- list(
-  OLS = train(formula, data = gss_train_tbl, method = "lm", trControl = train_control),
-  ElasticNet = train(formula, data = gss_train_tbl, method = "glmnet", trControl = train_control, tuneGrid = elastic_net_grid),
-  RandomForest = train(formula, data = gss_train_tbl, method = "rf", trControl = train_control, tuneGrid = random_forest_grid),
-  XGBoost = train(formula, data = gss_train_tbl, method = "xgbTree", trControl = train_control, tuneGrid = xgboost_grid)
+  OLS = train(formula, data = gss_train_tbl, method = "lm", 
+              trControl = train_control),
+  ElasticNet = train(formula, data = gss_train_tbl, method = "glmnet", 
+                     trControl = train_control, tuneGrid = elastic_net_grid),
+  RandomForest = train(formula, data = gss_train_tbl, method = "rf", 
+                       trControl = train_control, tuneGrid = random_f_grid),
+  XGBoost = train(formula, data = gss_train_tbl, method = "xgbTree", 
+                  trControl = train_control, tuneGrid = xgboost_grid)
 )
 # Print summaries of the models
 lapply(models, summary)
+
+# Publication
 
